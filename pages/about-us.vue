e8ecfd5210c64a3907d8bdb43f2c9060d1bee2cd
Adhitama <adhitamafikri@gmail.com>
2021-07-16 20:17:17 +0700

    feat: about us

---
+++ pages/about-us.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="about-us">
+    <h1 class="text-2xl text-center">About Us</h1>
+    <p class="text-center mt-8">This is a website for me to learn about SEO</p>
+  </section>
+</template>
